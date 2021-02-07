eca75020f5493930d721a26c00c9c1af75976f52
olsonea <olsonea@gmail.com>
2021-02-07 15:02:57 -0500

    Add useful write output messages

--- mid_server_download.ps1
+++ mid_server_download.ps1
@@ -1,14 +1,25 @@
-# Eg. User name="admin", Password="admin" for this code sample.
-$user = "integration.mid"
-$pass = "integration.mid"
+Param(
+    [parameter()][ValidateNotNullOrEmpty()][String]$username=$(throw "username is a mandatory parameter, please provide a value."),
+    [parameter()][ValidateNotNullOrEmpty()][String]$password=$(throw "password is a mandatory parameter, please provide a value."),
+    [parameter()][ValidateNotNullOrEmpty()][String]$instance=$(throw "instance is a mandatory parameter, please provide a value."),
+    [parameter()][ValidateNotNullOrEmpty()][String]$name=$(throw "name is a mandatory parameter, please provide a value."),
+    [parameter()][ValidateNotNullOrEmpty()][String]$operatingSystem=$(throw "operatingSystem is a mandatory parameter, please provide a value."),
+    [parameter()][ValidateNotNullOrEmpty()][String]$architecture=$(throw "architecture is a mandatory parameter, please provide a value.")
+)
 
-$instance = 'snow.heathcraft.com'
-$name = 'mid-windows-installer'
-$operatingSystem = 'windows'
-$architecture = 'x86-64'
+Write-Output $username, $instance #, $name, $operatingSystem, $architecture
+
+# Eg. User name="admin", Password="admin" for this code sample.
+#$username = "integration.mid"
+#$password = "integration.mid"
+
+#$instance = 'snow.heathcraft.com'
+#$name = 'mid-windows-installer'
+#$operatingSystem = 'windows'
+#$architecture = 'x86-64'
 
 # Build auth header
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $pass)))
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
 
 # Set proper headers
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
@@ -18,14 +29,17 @@ $headers.Add('Content-Type','application/json')
 
 # Specify endpoint uri
 #$uri = "https://" + $instance + "/api/1234/mid_server_installer?name=" + $name + "&operatingSystem=" + $operatingSystem + "&architecture=" + $architecture
-$uri = "https://" + $instance + "/api/1234/mid_server_installer
+$uri = "https://" + $instance + "/api/1234/mid_server_installer"
 
 # Specify HTTP method
 $method = "get"
 
 # Send HTTP request
 $installerURL = (Invoke-RestMethod -Headers $headers -Method $method -Uri $uri).result
+Write-Output "installerURL: " + $installerURL
+
 $filename = $installerURL.Substring($installerURL.LastIndexOf("/") + 1)
+Write-Output "filename: " + $filename
 
 # Print response
 $downloadInstaller = Invoke-RestMethod -Headers $headers -Method $method -Uri $installerURL -OutFile $filename
